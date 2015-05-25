6526c56e98290a521c0b5d6e08ded1244def0752
Elisa Herrmann <elisa.herrmann@atos.net>
2015-05-25 14:45:32 +0200

    add style

--- controllers/channels/channel.js
+++ controllers/channels/channel.js
@@ -38,172 +38,164 @@ var logger = require('../../logger.js');
 var AEONChannel = require('broker-manager').Channel;
 
 function checkChannelModel(channelModel) {
+  if (!("type" in channelModel) || (channelModel.type != "channel")) {
+    console.log("not model of type channel");
+    return false;
+  }
 
-    if (!("type" in channelModel) || (channelModel.type != "channel")) {
-        console.log("not model of type channel");
-        return false;
-    }
-    if (!("channelname" in channelModel) || !("channeldesc" in channelModel)) {
-        console.log("not channelname or channeldesc");
-        return false;
-    }
+  if (!("channelname" in channelModel) || !("channeldesc" in channelModel)) {
+    console.log("not channelname or channeldesc");
+    return false;
+  }
 
-    return true;
-
+  return true;
 }
 
 function checkSubscriptionParams(subscription) {
+  if (!("id" in subscription) || !("desc" in subscription)) {
+    console.log("incorrect subscription request");
+    return false;
+  }
 
-    if (!("id" in subscription) || !("desc" in subscription)) {
-        console.log("incorrect subscription request");        
-        return false;
-    }  
-
-    return true;  
+  return true;
 }
 
 function channelsResponse(err, doc, res) {
-
-    responsesManagment.sendResponse(err, res, doc)
+  responsesManagment.sendResponse(err, res, doc);
 }
 
 function addChannelPubSub(doc, req, res) {
+  // if (addChannelId === true)
+  //     channel_id = doc._id;
+  // else
+  //     channel_id = "";
 
-    // if (addChannelId === true)
-    //     channel_id = doc._id;
-    // else
-    //     channel_id = "";
+  var pubID = doc.pubID;
+  var subID = doc.subID;
+  var entityUrl = "";
 
-    var pubID = doc.pubID;
-    var subID = doc.subID;
+  if (config.app.extPort != 80) {
+    // entityUrl = config.app.protocol + "://" + config.app.extHost + ":" + config.app.extPort + req.url + channel_id;
+    entityUrl = config.app.protocol + "://" + config.app.extHost + ":" + config.app.extPort;
+  } else {
+    // entityUrl = config.app.protocol + "://" + config.app.extHost + req.url + channel_id;
+    entityUrl = config.app.protocol + "://" + config.app.extHost;
+  }
 
-    var entityUrl = "";
-    if (config.app.extPort != 80){
-        // entityUrl = config.app.protocol + "://" + config.app.extHost + ":" + config.app.extPort + req.url + channel_id;
-        entityUrl = config.app.protocol + "://" + config.app.extHost + ":" + config.app.extPort;
-    }
-    else{
-        // entityUrl = config.app.protocol + "://" + config.app.extHost + req.url + channel_id;
-        entityUrl = config.app.protocol + "://" + config.app.extHost;
-    }
-    doc.puburl = entityUrl + "/publish/"+pubID;
-    doc.subscriptionsurl = entityUrl + "/subscribe/"+subID;
+  doc.puburl = entityUrl + "/publish/" + pubID;
+  doc.subscriptionsurl = entityUrl + "/subscribe/" + subID;
 
-    delete doc.broker;
+  delete doc.broker;
 
-    channelsResponse(null, doc, res);
-
-
+  channelsResponse(null, doc, res);
 }
 
 
-exports.remove = function(req, res) {
+exports.remove = function (req, res) {
+  //Remove the channel from the database and the rabbitMQ
+  manager.deleteChannel(req.brokerConnection, req.dbConnection, req.params.entity, req.params.channel, function (err, doc) {
+    channelsResponse(err, doc, res);
+  });
+};
 
-    //Remove the channel from the database and the rabbitMQ
-    manager.deleteChannel(req.brokerConnection, req.dbConnection, req.params.entity, req.params.channel, function(err, doc) {
+exports.list = function (req, res) {
+  console.log("List of channels");
 
+  entities.isEntityOwner(req, function (checked, err) {
+    if (!checked) {
+      errorsManagement.sendError(err, res);
+    } else {
+      var projection = {
+        'channels.broker': 0,
+        'channels.subscriptions': 0
+      };
+
+      manager.findAllChannels(req.dbConnection, req.params.entity, projection, function (error, doc) {
         channelsResponse(err, doc, res);
-
-    });
-};
-
-exports.list = function(req, res) {
-    console.log("List of channels");
-
-    entities.isEntityOwner(req, function(checked, err) {
-        if (!checked)
-            errorsManagement.sendError(err, res);
-        else {
-            var projection = {
-                'channels.broker': 0,
-                'channels.subscriptions': 0
-            };
-
-            manager.findAllChannels(req.dbConnection, req.params.entity, projection, function(error, doc){
-                channelsResponse(err, doc, res);
-            });
-
-        }
-    });
-
-
-};
-
-
-exports.info = function(req, res) {
-    console.log("Info for channel ", req.params.channel);
-
-    entities.isEntityOwner(req, function(checked, error) {
-        if (!checked)
-            errorsManagement.sendError(error, res);
-        else
-            manager.getChannelInfo(req.params.entity, req.params.channel, req.dbConnection, function(error, doc) {
-                if (error){
-                    errorsManagement.sendError(errorsManagement.CHANNEL_NOT_EXISTS, res);
-                }else
-                    addChannelPubSub(doc, req, res);
-            });
-    });
-
-};
-
-exports.create = function(req, res) {
-
-    var channelModel;
-
-    function checkChannelCreated(err, doc){
-
-        if (err)
-            errorsManagement.sendError(errorsManagement.CHANNEL_ERROR, res);
-        else{
-            addChannelPubSub(doc, req, res);
-        }
+      });
 
     }
+  });
+};
 
-    function checkedEntityOwner(checked, err) {
-        if (!checked)
-            errorsManagement.sendError(err, res);
-        else
-            manager.createChannel(req.brokerConnection, req.dbConnection, req.params.entity, channelModel, checkChannelCreated );
+
+exports.info = function (req, res) {
+  console.log("Info for channel ", req.params.channel);
+
+  entities.isEntityOwner(req, function (checked, error) {
+    if (!checked) {
+      errorsManagement.sendError(error, res);
+    } else {
+      manager.getChannelInfo(req.params.entity, req.params.channel, req.dbConnection, function (error, doc) {
+        if (error) {
+          errorsManagement.sendError(errorsManagement.CHANNEL_NOT_EXISTS, res);
+        } else {
+          addChannelPubSub(doc, req, res);
+        }
+      });
+    }
+  });
+
+};
+
+exports.create = function (req, res) {
+  var channelModel;
+
+  function checkChannelCreated(err, doc) {
+    if (err) {
+      errorsManagement.sendError(errorsManagement.CHANNEL_ERROR, res);
+    } else {
+      addChannelPubSub(doc, req, res);
     }
 
-    logger.info("createChannel()");
+  }
 
-    channelModel = req.body;
+  function checkedEntityOwner(checked, err) {
+    if (!checked) {
+      errorsManagement.sendError(err, res);
+    } else {
+      manager.createChannel(req.brokerConnection, req.dbConnection, req.params.entity, channelModel, checkChannelCreated);
+    }
+  }
 
-    if(channelModel.channelname == "" || channelModel.channelname == undefined)
-             errorsManagement.sendError(errorsManagement.WRONG_CHANNEL_ID,  res);
+  logger.info("createChannel()");
 
-    if (checkChannelModel(channelModel)) {
-        logger.info("Create channel into broker");
-        entities.isEntityOwner(req, checkedEntityOwner);
-    } else
-        errorsManagement.sendError(errorsManagement.INCORRECT_MODEL_ERROR, 400, res);
+  channelModel = req.body;
 
-}
+  if (channelModel.channelname == "" || channelModel.channelname == undefined) {
+    errorsManagement.sendError(errorsManagement.WRONG_CHANNEL_ID,  res);
+  }
 
-exports.update = function(req, res){
+  if (checkChannelModel(channelModel)) {
+    logger.info("Create channel into broker");
+    entities.isEntityOwner(req, checkedEntityOwner);
+  } else {
+    errorsManagement.sendError(errorsManagement.INCORRECT_MODEL_ERROR, 400, res);
+  }
 
-    var entityID = req.params.entity;
+};
 
-    var channelID = req.params.channel;
+exports.update = function (req, res) {
+  var entityID = req.params.entity;
+  var channelID = req.params.channel;
+  var channelModel = req.body;
 
-    var channelModel = req.body;
+  if (checkChannelModel(channelModel)) {
+    logger.info("Update channel");
 
-    if (checkChannelModel(channelModel)) {
-        logger.info("Update channel");
+    manager.updateChannel(entityID, channelID, req.body, req.dbConnection, function (err, doc) {
+      if (err) {
+        errorsManagement.sendError(errorsManagement.CHANNEL_ERROR, res);
+      } else {
+        channelsResponse(err, doc, res);
+      }
 
-        manager.updateChannel(entityID, channelID, req.body, req.dbConnection, function(err, doc){
-            if (err)
-                errorsManagement.sendError(errorsManagement.CHANNEL_ERROR, res);
-            else
-                channelsResponse(err, doc, res);
-        });
-    } else
-        errorsManagement.sendError(errorsManagement.INCORRECT_MODEL_ERROR, res);
+    });
+  } else {
+    errorsManagement.sendError(errorsManagement.INCORRECT_MODEL_ERROR, res);
+  }
 
-}
+};
 
 /*
  * Preconditions:
@@ -214,29 +206,30 @@ exports.update = function(req, res){
  * to publish the messagePublish
  *
  */
-exports.publish = function(req, res, next) {
+exports.publish = function (req, res, next) {
 
-    logger.info("publish()");
-    var doc = null;
+  logger.info("publish()");
+  var doc = null;
 
-    if ("AEONChannel" in req) {
-        doc = req.AEONChannel;
+  if ("AEONChannel" in req) {
+    doc = req.AEONChannel;
 
-        var channel = new AEONChannel(doc.channelname);
+    var channel = new AEONChannel(doc.channelname);
 
-        channel.setChannelDesc(doc.channeldesc);
-        channel.setPubKey(doc.broker.pubkey);        
-        channel.setPubID(doc.pubID);
-        channel.setSubID(doc.subID);
+    channel.setChannelDesc(doc.channeldesc);
+    channel.setPubKey(doc.broker.pubkey);
+    channel.setPubID(doc.pubID);
+    channel.setSubID(doc.subID);
 
-        manager.publish(req.brokerConnection, channel, req.body, function(err, doc) {
-            channelsResponse(err, req.body, res);
-            next(); //Invoke the next worker of the chain
-        });
-    } else
-        errorsManagement.sendError(errorsManagement.UNKNOWN_ERROR, res);
+    manager.publish(req.brokerConnection, channel, req.body, function (err, doc) {
+      channelsResponse(err, req.body, res);
+      next(); //Invoke the next worker of the chain
+    });
+  } else {
+    errorsManagement.sendError(errorsManagement.UNKNOWN_ERROR, res);
+  }
 
-}
+};
 
 
 
@@ -249,40 +242,42 @@ exports.publish = function(req, res, next) {
  * broker and the information is attached to the channel document
  *
  */
-exports.getSubscription = function(req, res, next) {
+exports.getSubscription = function (req, res, next) {
+  logger.info("getSubscription()");
 
-    logger.info("getSubscription()");
+  if (checkSubscriptionParams(req.query)) {
+    if ("AEONChannel" in req) {
+      var doc = req.AEONChannel;
+      var channel = new AEONChannel(doc.channelname);
 
-    if (checkSubscriptionParams(req.query)) {
-        if ("AEONChannel" in req) {
-            var doc = req.AEONChannel;
+      channel.setChannelDesc(doc.channeldesc);
+      channel.setPubKey(doc.broker.pubkey);
+      channel.setPubID(doc.pubID);
+      channel.setSubID(doc.subID);
 
-            var channel = new AEONChannel(doc.channelname);
-            channel.setChannelDesc(doc.channeldesc);
-            channel.setPubKey(doc.broker.pubkey);            
-            channel.setPubID(doc.pubID);
-            channel.setSubID(doc.subID);
+      console.log("creating subscription for channel " + channel.getChannelName() + " and subID " + channel.getSubID());
 
-            console.log("creating subscription for channel " + channel.getChannelName() + " and subID " + channel.getSubID());
+      var id = req.query["id"];
+      var desc = req.query["desc"];
+      var ip = req.headers['host'];
 
-            var id = req.query["id"];
-            var desc = req.query["desc"];
-            var ip = req.headers['host'];
+      subscription = {
+        "id": id,
+        "desc": desc,
+        "ip": ip
+      };
 
-            subscription = {
-                "id": id,
-                "desc": desc,
-                "ip": ip
-            };
+      manager.createQueue(req.brokerConnection, req.dbConnection, req.entityID, req.channelID, channel, subscription, function (error, doc) {
+        channelsResponse(error, doc, res);
+        next(); //Invoke the next worker of the chain
+      });
 
-            manager.createQueue(req.brokerConnection, req.dbConnection, req.entityID, req.channelID, channel, subscription, function(error, doc){
-                channelsResponse(error, doc, res);
-                next(); //Invoke the next worker of the chain
-            });
+    } else {
+      errorsManagement.sendError(errorsManagement.UNKNOWN_ERROR, res);
+    }
+  } else {
+    errorsManagement.sendError(errorsManagement.INCORRECT_MODEL_ERROR, res);
+  }
 
-        } else
-            errorsManagement.sendError(errorsManagement.UNKNOWN_ERROR, res);
-    } else
-        errorsManagement.sendError(errorsManagement.INCORRECT_MODEL_ERROR, res);
-}
+};
 
